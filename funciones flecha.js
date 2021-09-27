f98aa47443ba5b6a27fb36ccc7d0e1e115b33054
Scris2142345 <89808626+Scris2142345@users.noreply.github.com>
2021-09-27 16:04:15 -0500

    Aprendiendo funcion flecha

---
+++ funciones flecha.js
@@ -0,0 +1,51 @@
+var num1;
+var num2;
+
+function Saludar(){
+  
+    alert("Hola a todos");
+  }
+
+  // //
+
+
+function Restarimpares(num1,num2){
+
+    var resultado= num1 + num2;
+    while (n < 2){
+        n ++;
+        x + n;
+    }
+    if (resultado = ){
+        console.log("el resultado es:, var resultado");
+     } else{
+         console.log("Error solo numeros impares");
+        }
+
+
+}
+console.log();
+
+// // 
+
+var noombredelafuncion = (vector, tam) => {
+    for (let i = 0; i < tam; ++i){
+        vector.push(math.round(mathc))
+    }
+    if (){
+} else{
+
+}
+ return (nombredelafuncion());
+}
+console.log("");
+
+// //
+
+function Sumarpares(num1,num2){
+  
+    var resultado= num1 + num2;
+    alert(“La suma es: ”+ resultado);
+  }
+  
+  Sumar(8,9);
